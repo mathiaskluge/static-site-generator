a4d3ee3b51bf523d5f60a0530c0dc67d948fd178
mk <59650843+mathiaskluge@users.noreply.github.com>
2024-05-22 12:00:26 +0200

    Add tests for TextNode

--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,12 @@
 class TextNode:
 
-    def __init__(self, text: str, text_type: str, url: str) -> None:
+    def __init__(self, text: str, text_type: str, url=None) -> None:
         self.text = text
         self.text_type = text_type
         self.url = url
 
-    def __eq__(self) -> bool:
-        return self.text == self.text_type == self.url
+    def __eq__(self, other) -> bool:
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
