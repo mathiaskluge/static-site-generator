0ab72adaee9cabfe8811980e313532890c1e08dc
mk <59650843+mathiaskluge@users.noreply.github.com>
2024-05-24 21:26:03 +0200

    Minor documentation and file clean up

--- src/main.py
+++ src/main.py
@@ -2,7 +2,13 @@ import os
 import shutil
 
 
-def copy_files(src: str = "static", dst: str = "public",fil_c: int = 0, fol_c: int = 0):
+def copy_files(
+        src: str = "static",
+        dst: str = "public",
+        fil_c: int = 0,
+        fol_c: int = 0
+        ):
+    """Copies files and folders from a src into a dst directory."""
     src_path = f"{src}/"
     dst_path = f"{dst}/"
 
@@ -13,20 +19,22 @@ def copy_files(src: str = "static", dst: str = "public",fil_c: int = 0, fol_c: i
         # Remove old dst dir and all files
         shutil.rmtree(dst_path, ignore_errors=True)
         # Recreate the destination directory
+        os.makedirs(dst_path)
+        # log folders
         if fol_c == 0:
             print(f"🗂️ {dst_path}")
         else:
             print(f"|{fol_c * "-"} 🗂️ {dst_path}")
-        os.makedirs(dst_path)
 
+        # recursive copy
         for src_file in os.listdir(src_path):
             src_file_path = f"{src_path}{src_file}"
             dst_file_path = f"{dst_path}{src_file}"
-
             # is file
             if os.path.isfile(src_file_path):
                 fil_c += 1
                 shutil.copy(src_file_path, dst_file_path)
+                # log files
                 if fol_c == 0:
                     print(f"|- 📄{src_file}")
                 else:
@@ -34,7 +42,12 @@ def copy_files(src: str = "static", dst: str = "public",fil_c: int = 0, fol_c: i
             # is folder
             else:
                 fol_c += 1
-                copy_files(f"{src_file_path}", f"{dst_file_path}", fil_c, fol_c)
+                copy_files(
+                    f"{src_file_path}",
+                    f"{dst_file_path}",
+                    fil_c,
+                    fol_c
+                    )
                 fol_c -= 1
 
     except Exception as e:
